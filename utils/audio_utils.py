384b882be08df192feb6528d697bc0eb396ab3c1
Jordan Henshaw <162933818+jordanhenshaw@users.noreply.github.com>
2024-10-19 12:45:24 -0400

    3D Audio: Irregular Sound Object Scaling Improvement
    Now handles sound objects with asymnetric scaling better. Currently, user must add vertices if a speaker is in the middle of a mesh for it to hit correctly. This type of thing is great for beds.
    
    In Atmos, sound objects and beds are totally different things. In PanLab I don't think you have beds at all.
    
    With this, you can think of a "bed" as just a large 3D audio object. This is a far simpler, more first-principlesy way to think about it. "Bed" is just a made up word that is there only because Atmos can't do asymnetric scaling. Sorcerer can.

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -15,9 +15,19 @@ REMAP_MINIMUM = -59
 def render_volume(speaker, sound_object, audio_cue):
     '''Basically a crude form of the Dolby Atmos Renderer'''
     sound_object_world_location = sound_object.matrix_world.to_translation()
+
+    scale_x, scale_y, scale_z = sound_object.scale
+    if not round(scale_x, 2) == round(scale_y, 2) == round(scale_z, 2):
+        # Get the closest vertex to the speaker if the scale is incongruent
+        closest_vertex = find_closest_vertex_to_speaker(speaker, sound_object)
+        sound_object_world_location = closest_vertex
+        adjusted_multiplier = 1
+    else:
+        adjusted_multiplier = SPEAKER_SCALE_MULTIPLIER
+
     distance = round((speaker.location - sound_object_world_location).length, 2)
 
-    scale_factor = SPEAKER_SCALE_MULTIPLIER * ((speaker.scale[0] + speaker.scale[1] + speaker.scale[2]) / 3) * \
+    scale_factor = adjusted_multiplier * ((speaker.scale[0] + speaker.scale[1] + speaker.scale[2]) / 3) * \
                    ((sound_object.scale[0] + sound_object.scale[1] + sound_object.scale[2]) / 3)
 
     adjusted_distance = max(distance / scale_factor, 1e-6)  # Division reduces the impact of scale on attenuation
@@ -42,6 +52,24 @@ def render_volume(speaker, sound_object, audio_cue):
 
     return volume
 
+
+def find_closest_vertex_to_speaker(speaker, sound_object):
+    '''Find the vertex closest to the speaker in world space'''
+    object_mesh = sound_object.data  # Get the mesh data
+    closest_vertex = None
+    min_distance = float('inf')
+
+    # Iterate through the vertices
+    for vertex in object_mesh.vertices:
+        vertex_world_position = sound_object.matrix_world @ vertex.co
+        distance_to_speaker = (speaker.location - vertex_world_position).length
+        
+        if distance_to_speaker < min_distance:
+            min_distance = distance_to_speaker
+            closest_vertex = vertex_world_position
+
+    print(f"Vertex: {closest_vertex}")
+    return closest_vertex
 
 
 def publish_volume(channel, parameter, value):
